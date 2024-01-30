4c186f03038f66a0e0f83c68946c1f8243665f3b
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-01-30 14:48:53 +0100

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel</title>
+</head>
+<body>
+    <header>
+        <h1>
+            Hello World
+        </h1>
+        <p>
+            {{ $title }} , {{ $user }}
+        </p>
+    </header>
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $title = 'Ciao a tutti!';
+    return view('home', [
+                            'title' => $title,
+                            'user' => 'Si'
+                        ]);
+});
+
+Route::get('/Casa', function () {
+    
+    return view('casa');
+});
